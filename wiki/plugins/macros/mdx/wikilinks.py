4a5e6665650faa24dc5c47722785f7b066d490e8
Ronald White <ouyanghongyu@gmail.com>
2015-09-10 20:39:27 +0800

    wikilink's [[link_path|Link Display]] implemented.

--- wiki/plugins/macros/mdx/wikilinks.py
+++ wiki/plugins/macros/mdx/wikilinks.py
@@ -12,14 +12,6 @@ import re
 def build_url(label, base, end, md):
     """ Build a url from the label, a base, and an end. """
     clean_label = re.sub(r'([ ]+_)|(_[ ]+)|([ ]+)', '_', label)
-    urlpaths = md.article.urlpath_set.all()
-    # Nevermind about the base we are fed, just keep the original
-    # call pattern from the wikilinks plugin for later...
-    base = '/'
-    for urlpath in urlpaths:
-        if urlpath.children.filter(slug=clean_label).exists():
-            base = ''
-            break
     return '%s%s%s' % (base, clean_label, end)
 
 
@@ -42,7 +34,8 @@ class WikiLinkExtension(wikilinks.WikiLinkExtension):
         self.md = md
 
         # append to end of inline patterns
-        WIKILINK_RE = r'\[\[([\w0-9_ -]+)\]\]'
+        # WIKILINK_RE = r'\[\[([\w0-9_ -]+)\]\]'
+        WIKILINK_RE = r'\[\[(([\w0-9_ -]+)|([\w0-9_ -]+)\|([\w0-9_ -]+))\]\]'
         wikilinkPattern = WikiLinks(WIKILINK_RE, self.getConfigs())
         wikilinkPattern.md = md
         md.inlinePatterns.add('wikilink', wikilinkPattern, "<not_strong")
@@ -51,15 +44,16 @@ class WikiLinkExtension(wikilinks.WikiLinkExtension):
 class WikiLinks(wikilinks.WikiLinks):
 
     def handleMatch(self, m):
-        if m.group(2).strip():
-            base_url, end_url, html_class = self._getMeta()
-            label = m.group(2).strip()
-            url = self.config['build_url'](label, base_url, end_url, self.md)
-            a = markdown.util.etree.Element('a')
-            a.text = label
-            a.set('href', url)
-            if html_class:
-                a.set('class', html_class)
+        base_url, end_url, html_class = self._getMeta()
+        sub_url = m.group(4) or m.group(2)
+        sub_url = sub_url.strip()
+        url = self.config['build_url'](sub_url, base_url, end_url, self.md)
+        a = markdown.util.etree.Element('a')
+        a.text = m.group(5) or m.group(3)
+        a.text = a.text.strip()
+        a.set('href', url)
+        if html_class:
+            a.set('class', html_class)
         else:
             a = ''
         return a
